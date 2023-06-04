a00f912d2184e07a54d1bb9ea74a4042610f4271
Keyanna Mayfield <11904976+keyannam@users.noreply.github.com>
2023-06-04 09:28:50 -0500

    Loads data into redux

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,10 @@ import Loading from './Loading';
 import { 
   loadProvider,
   loadNetwork,
-  loadAccount  
+  loadAccount,
+  loadTokens,
+  loadAMM
 } from '../store/interactions'
-
-// ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
-
-// Config: Import your network config here
-// import config from '../config.json';
 
 function App() {
 
@@ -31,6 +27,10 @@ function App() {
 
     // Fetch accounts
     await loadAccount(dispatch)
+
+    // Initiate contracts
+    await loadTokens(provider, chainId, dispatch)
+    await loadAMM(provider, chainId, dispatch)
   }
 
   useEffect(() => {
